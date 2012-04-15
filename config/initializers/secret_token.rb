241b0d67f62a0fd4349fdb19c72e0d08af2bf1f6
Alexander Bradner <alexb@intersect.org.au>
2012-04-15 19:04:36 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chartracker::Application.config.secret_token = 'baf07af74829f58dc0c79d0caef931b40d524ad22d660b98aeff255e04c5d017e39797c1346ba5c169780ab4c1e9874ff7b62b7df7ec0e9a8f90ebaf4baa63fc'
